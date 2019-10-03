04a6dbee58e8e2ec8f5a4355bebc29979d3bbe1d
Deva <54841284+DasariDevaSuryaSainath@users.noreply.github.com>
2019-10-03 19:01:10 +0530

    Binary search tree

--- main.c
+++ main.c
@@ -1,39 +1,76 @@
-#include <assert.h>
-#include "double.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+#include<assert.h>
+#include "bst.c"
+
 
 int main()
 {
-//10.   Create double linked list with methods to add, remove, to check the existence of element.
+Tree bst = tree_new();
+Tree *tree = &bst;
+//1. Design a BST class with methods to add element, search element, number of elements and delete requested element
+tree = add_node(tree,50);
+tree = add_node(tree,20);
+tree = add_node(tree,30);
+tree = add_node(tree,60);
+tree = add_node(tree,70);
+tree = add_node(tree,35);
+tree = add_node(tree,40);
+
+assert(tree->count == 7);
+
+assert(lookup(tree,50)==1);
+assert(lookup(tree,90)==0);
+
+tree = delete_node(tree,35);
+assert(tree->count == 6);
+
+tree = delete_node(tree,35);
+
+
+//2. Add methods to in-order, pre-order, post-order and level-order traversals
+printf("In-order: ");
+tree_inorder(tree); 
+printf("\nPre-order: ");
+tree_preorder(tree);
+printf("\nPost-order: ");
+tree_postorder(tree);
+
+//3. Add method to find the height of binary search tree
+
+assert(tree_height(tree)==4);
+
+//4. Add method to count the number of terminal nodes in BST
+
+assert(terminal_nodes(tree)==2);
+tree = add_node(tree,25);
+
+assert(terminal_nodes(tree)==3);
+tree = add_node(tree,55);
+
+assert(terminal_nodes(tree)==4);
+tree = add_node(tree,58);
+assert(terminal_nodes(tree)==4);
+
+
+//5. Add methods to find max and min element in BST
+
+assert(find_min(tree)==20);
+assert(find_max(tree)==70);
+
+//6. Add method to display elements in ascending order
+printf("\n");
+printf("Ascending Order: ");
+tree_ascending(tree);
+//7. Add method to display elements in descending order
+
+printf("\n");
+printf("Descending Order: "); 
+tree_descending(tree); 
 
 
 
-	Slist s = slist_new();
-	Slist *list = &s;
+return 0 ;
 
-	list=slist_add_tail(list,10);
-	list=slist_add_tail(list,20);
-	list=slist_add_tail(list,30);
-	list=slist_add_tail(list,40);
-
-	list=slist_delete_tail(list);
-	list=slist_delete_tail(list);
-
-	assert(slist_lookup(list,20)==1);
-	assert(slist_lookup(list,80)==0);
-
-	slist_add_head(list,30);
-	slist_add_head(list,40);
-
-	slist_delete_head(list);
-	assert(slist_length(list)!=2);
-	slist_delete_head(list);
-	slist_delete_head(list);
-
-	slist_delete_head(list);
-
-	assert(slist_length(list)==0);
-	slist_add_head(list,30);
-	
-	assert(slist_length(list)==1);
-	return 0;
 }
